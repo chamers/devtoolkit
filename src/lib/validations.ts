9257feb9a3ea5797e1013c19ab2de0405a4785d1
Christel Hamers <christelhamers@gmail.com>
2025-08-18 07:59:07 +0100

    set up initiatl auth for signin/out with credentials

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 export const signUpSchema = z.object({
-  userName: z.string().min(1, "User name is required"),
+  userName: z.string().min(1, "Username is required"),
   email: z.string().email("Enter a valid email"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
