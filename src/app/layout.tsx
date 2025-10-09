8b681b3ed4aa7d189be9bb0b1a329ffd6f70fba2
Christel Hamers <christelhamers@gmail.com>
2025-10-09 17:33:05 +0100

    replace all occurrences of /fonts/.... (absolute path) with ./fonts/... (relative path) + create global.d.ts file to suppress import ./globals.css warning in layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,49 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Providers  from "./providers";
+import Providers from "./providers";
 import localFont from "next/font/local";
-
 
 const poppins = localFont({
   src: [
-    { path: "/fonts/Poppins-Thin.ttf", weight: "100", style: "normal" },
-    { path: "/fonts/Poppins-ThinItalic.ttf", weight: "100", style: "italic" },
-    { path: "/fonts/Poppins-ExtraLight.ttf", weight: "200", style: "normal" },
-    { path: "/fonts/Poppins-ExtraLightItalic.ttf", weight: "200", style: "italic" },
-    { path: "/fonts/Poppins-Light.ttf", weight: "300", style: "normal" },
-    { path: "/fonts/Poppins-LightItalic.ttf", weight: "300", style: "italic" },
-    { path: "/fonts/Poppins-Regular.ttf", weight: "400", style: "normal" },
-    { path: "/fonts/Poppins-Medium.ttf", weight: "500", style: "normal" },
-    { path: "/fonts/Poppins-MediumItalic.ttf", weight: "500", style: "italic" },
-    { path: "/fonts/Poppins-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "/fonts/Poppins-SemiBoldItalic.ttf", weight: "600", style: "italic" },
-    { path: "/fonts/Poppins-Bold.ttf", weight: "700", style: "normal" },
-    { path: "/fonts/Poppins-BoldItalic.ttf", weight: "700", style: "italic" },
-    { path: "/fonts/Poppins-ExtraBold.ttf", weight: "800", style: "normal" },
-    { path: "/fonts/Poppins-ExtraBoldItalic.ttf", weight: "800", style: "italic" },
-    { path: "/fonts/Poppins-Black.ttf", weight: "900", style: "normal" },
-    { path: "/fonts/Poppins-BlackItalic.ttf", weight: "900", style: "italic" },
+    { path: "./fonts/Poppins-Thin.ttf", weight: "100", style: "normal" },
+    { path: "./fonts/Poppins-ThinItalic.ttf", weight: "100", style: "italic" },
+    { path: "./fonts/Poppins-ExtraLight.ttf", weight: "200", style: "normal" },
+    {
+      path: "./fonts/Poppins-ExtraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    { path: "./fonts/Poppins-Light.ttf", weight: "300", style: "normal" },
+    { path: "./fonts/Poppins-LightItalic.ttf", weight: "300", style: "italic" },
+    { path: "./fonts/Poppins-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/Poppins-Medium.ttf", weight: "500", style: "normal" },
+    {
+      path: "./fonts/Poppins-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    { path: "./fonts/Poppins-SemiBold.ttf", weight: "600", style: "normal" },
+    {
+      path: "./fonts/Poppins-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    { path: "./fonts/Poppins-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/Poppins-BoldItalic.ttf", weight: "700", style: "italic" },
+    { path: "./fonts/Poppins-ExtraBold.ttf", weight: "800", style: "normal" },
+    {
+      path: "./fonts/Poppins-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    { path: "./fonts/Poppins-Black.ttf", weight: "900", style: "normal" },
+    { path: "./fonts/Poppins-BlackItalic.ttf", weight: "900", style: "italic" },
   ],
-  display: "swap"
+  display: "swap",
 });
 
 const crimsonPro = localFont({
-  src: [
-    { path: "./fonts/CrimsonPro-VariableFont_wght.ttf", style: "normal" }],
+  src: [{ path: "./fonts/CrimsonPro-VariableFont_wght.ttf", style: "normal" }],
   display: "swap",
   variable: "--serif-regular",
 });
@@ -41,13 +55,20 @@ const robotMono = localFont({
 });
 
 const robotoCondensed = localFont({
-  src: [{ path: "./fonts/RobotoCondensed-VariableFont_wght.ttf", style: "normal" }],
+  src: [
+    { path: "./fonts/RobotoCondensed-VariableFont_wght.ttf", style: "normal" },
+  ],
   display: "swap",
   variable: "--font-condensed",
 });
 
 const GaramondItalic = localFont({
-  src: [{ path: "./fonts/EBGaramond-Italic-VariableFont_wght.ttf", style: "italic" }],
+  src: [
+    {
+      path: "./fonts/EBGaramond-Italic-VariableFont_wght.ttf",
+      style: "italic",
+    },
+  ],
   display: "swap",
   variable: "--serif-italic",
 });
@@ -58,10 +79,10 @@ const caveat = localFont({
   variable: "--font-hand",
 });
 
-
 export const metadata: Metadata = {
   title: "DevToolkit: Your All-in-One Web Dev Resources Hub",
-  description: "DevToolkit empowers developers of all levels to build web applications faster and easier. It provides a comprehensive suite of tools for front-end development, back-end development, digital design and code management.",
+  description:
+    "DevToolkit empowers developers of all levels to build web applications faster and easier. It provides a comprehensive suite of tools for front-end development, back-end development, digital design and code management.",
 };
 
 export default function RootLayout({
@@ -71,10 +92,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${poppins.className} ${crimsonPro.variable} ${robotMono.variable} ${robotoCondensed.variable} ${caveat.variable} ${GaramondItalic.variable} antialiased`}>
-        <Providers>
-         {children}
-        </Providers>
+      <body
+        className={`${poppins.className} ${crimsonPro.variable} ${robotMono.variable} ${robotoCondensed.variable} ${caveat.variable} ${GaramondItalic.variable} antialiased`}
+      >
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
