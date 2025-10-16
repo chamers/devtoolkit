14a821848ac4a100eea22c9ab925da7d9f95e613
Christel Hamers <christelhamers@gmail.com>
2025-10-16 16:24:22 +0100

    Update ResourceImage.tsx to imagekit Image element

--- src/components/ResourceImage.tsx
+++ src/components/ResourceImage.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { LogoUrl } from "@/lib/types";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+// import Image from "next/image";
+import { Image } from "@imagekit/next";
+import config from "@/lib/config";
 
 type ResourceImageVariant =
   | "extraSmall"
@@ -39,11 +42,38 @@ const ResourceImage = ({
         className="absolute z-10"
         style={{ left: "12%", width: "87.5%", height: "88%" }}
       >
-        <Image
+        {/* <Image
           src={logoUrl}
           alt="Resource Image"
           fill
           className="rounded-sm object-fill"
+        /> */}
+
+        {/* <IKImage
+          path={logoUrl}
+          urlEndpoint={config.env.imagekit.urlEndpoint}
+          alt="Resource Image"
+          fill
+          className="rounded-sm object-fill"
+          loading="lazy"
+          lqip={{ active: true }}
+        />
+        <Image
+      urlEndpoint="https://ik.imagekit.io/your_imagekit_id" // New prop
+      src="/profile.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    /> */}
+        <Image
+          // ✅ This is the ImageKit Next.js component
+          src={logoUrl} // can be relative or absolute
+          urlEndpoint={config.env.imagekit.urlEndpoint}
+          alt="Resource Image"
+          fill // uses next/image-style layout
+          className="rounded-sm object-cover"
+          loading="lazy"
+          transformation={[{ width: 400, height: 400 }]} // optional
         />
       </div>
     </div>
