2f111c36ee1139ac1c20498884ed0c873563922f
Christel Hamers <christelhamers@gmail.com>
2026-01-13 08:46:47 +0000

    add component UpdateUserForm to components/admin/forms

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,3 +1,4 @@
+import UpdateUserForm from "@/components/admin/forms/update-user-form";
 import SignOut from "@/components/auth/sign-out";
 import ReturnButton from "@/components/return-button";
 import { Button } from "@/components/ui/button";
@@ -49,18 +50,27 @@ export default async function Page() {
       {session.user.image ? (
         <img
           src={session.user.image}
-          alt="Profile"
-          className="w-16 h-16 rounded-full"
+          alt="Profile Image"
+          className="w-16 h-16 border border-primary rounded-md object-cover"
         />
       ) : (
-        <div className="size-32 border border-primary rounded-md bg-primary text-primary-foreground flex items-center justify-center">
-          {session.user.name.slice(0, 2)}
+        <div className="size-24 border border-primary rounded-md bg-primary text-primary-foreground flex items-center justify-center">
+          <span className="uppercase text-lg font-bold">
+            {session.user.name.slice(0, 2)}
+          </span>
         </div>
       )}
 
       <pre className="text-sm overflow-clip">
         {JSON.stringify(session, null, 2)}
       </pre>
+      <div className="space-y-4 p-4 rounded-b-md border border-t-8 border-blue-600">
+        <h2>Update User</h2>
+        <UpdateUserForm
+          name={session.user.name}
+          image={session.user.image ?? ""}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/admin/forms/update-user-form.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface UpdateUserFormProps {
+  name: string;
+  image: string;
+}
+
+const UpdateUserForm = ({ name, image }: UpdateUserFormProps) => {
+  return <div>UpdateUserForm</div>;
+};
+export default UpdateUserForm;
