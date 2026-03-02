58a5712bb6b5ed91fa5bfbc0d7150d5107b43465
Christel Hamers <christelhamers@gmail.com>
2026-03-02 09:48:00 +0000

    debug why profile page is denied to user with approved status

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import GetStartedButton from "@/components/get-started-button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-const LandingPage = () => {
+const LandingPage = async () => {
+  const h = await headers();
+  const session = await auth.api.getSession({ headers: h });
   return (
     <section className="py-16 flex justify-center h-screen">
+      {JSON.stringify(session, null, 2)}
       <div className="text-center max-w-3xl">
         <h1 className="text-4xl font-semibold tracking-tight">
           Welcome to DevToolkit
